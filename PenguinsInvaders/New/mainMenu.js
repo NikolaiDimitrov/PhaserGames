0c99879070af79802562fec41d9a9a7a7cf093ee
NikolaiDimitrov <nickolai_dimitrov@abv.bg>
2015-05-29 16:18:43 +0300

    Update mainMenu.js

--- PenguinsInvaders/New/mainMenu.js
+++ PenguinsInvaders/New/mainMenu.js
@@ -1,8 +1,6 @@
 
 BasicGame.MainMenu = function (game) {
 
-  //this.music = null;
-  //this.playButton = null;
 
 };
 
@@ -10,9 +8,7 @@ BasicGame.MainMenu.prototype = {
 
   create: function () {
 
-    //  We've already preloaded our assets, so let's kick right into the Main Menu itself.
-    //  Here all we're doing is playing some music and adding a picture and button
-    //  Naturally I expect you to do something significantly better :)
+ 
 
     this.add.sprite(0, 0, 'titlepage');
     console.log("Stiga li do tuka ??");
